ce1067281a38af5714010c05c3216b59859f4819
Waltoonn <leha.messi@bk.ru>
2024-09-21 17:58:03 -0100

    саня лох

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ bot = Bot(my_token)
 dp = Dispatcher(bot)
 
 async def on_startup(_):
-    print('Я работаю епта')
+    print('Я работаю')
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await message.answer('')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
