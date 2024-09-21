fa4a964a68d54ef6f3ed64bc82d7a0fa497b493c
Waltoonn <leha.messi@bk.ru>
2024-09-21 19:23:12 -0100

    'adsdsa'

--- inlinebutton.py
+++ inlinebutton.py
@@ -4,4 +4,6 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 ikb_start = InlineKeyboardMarkup(inline_keyboard=([
     [InlineKeyboardButton('Материалы с лекций', callback_data='materials_from_lectures')],
     [InlineKeyboardButton('Готовое ДЗ', callback_data='ready_made_dz')],
+    [InlineKeyboardButton('Профиль', callback_data='profile')],
+    [InlineKeyboardButton('Реферальная система', callback_data='referal_system')]
 ]))
